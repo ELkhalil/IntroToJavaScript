398096cfb2ffee0b0526e5d1a97a263085dc9866
Abderrahman Elkhalil <aelkhali@e1r2p1.1337.ma>
2024-01-03 12:55:32 +0100

    getting familiar with javascript syntax

---
+++ experiment.js
@@ -0,0 +1,23 @@
+// console.log(12 + 3); //15
+// console.log(4 - 3); //1
+// console.log(5 * 3); //15
+// console.log(8 % 3); //2
+// console.log(4 / 2); //2
+
+// //concationation of two strings output: Abderrahman Elkhalil
+// console.log("Abderrahman" + "Elkhalil");
+
+// var numbers = [23, 65, 78];
+// console.log(numbers[0]); 
+
+const person = {
+    name: "Brian Holt",
+    city: "Seattle",
+    state: "WA",
+    favoriteFood: "🌮",
+    wantsTacosRightNow: true,
+    numberOfTacosWanted: 100,
+  };
+  console.log(person);
+  console.log(person.name);
+  console.log(person["name"]); // same as the line above; prefer using the other one
